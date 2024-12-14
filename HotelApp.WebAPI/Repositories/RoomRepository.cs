61f65251341ef40ae3e7c41bf37fa9aa0b3f62b1
Cong Dinh <congdinh2012@hotmail.com>
2024-12-14 22:42:08 +0700

    NEFC - Task 02 - Step 06: Create Room Repository with CRUD

---
+++ HotelApp.WebAPI/Repositories/RoomRepository.cs
@@ -0,0 +1,48 @@
+using HotelApp.WebAPI.Data;
+using HotelApp.WebAPI.Models;
+
+namespace HotelApp.WebAPI.Repositories;
+
+public class RoomRepository(HotelAppDbContext context) : IRoomRepository
+{
+    private readonly HotelAppDbContext _context = context;
+
+    public IEnumerable<Room> GetAll()
+    {
+        return [.. _context.Rooms];
+    }
+
+    public Room? GetById(Guid id)
+    {
+        return _context.Rooms.Find(id);
+    }
+
+    public int Add(Room entity)
+    {
+        _context.Rooms.Add(entity);
+        return _context.SaveChanges();
+    }
+
+    public bool Update(Room entity)
+    {
+        _context.Rooms.Update(entity);
+        return _context.SaveChanges() > 0;
+    }
+
+    public bool Delete(Guid id)
+    {
+        var room = _context.Rooms.Find(id);
+        if (room == null)
+        {
+            return false;
+        }
+        _context.Rooms.Remove(room);
+        return _context.SaveChanges() > 0;
+    }
+
+    public bool Delete(Room entity)
+    {
+        _context.Rooms.Remove(entity);
+        return _context.SaveChanges() > 0;
+    }
+}
